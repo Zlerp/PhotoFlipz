e1a400c543cc788e32a0c17368a5fe2b7dc02404
Zlerp <zhpdeals@gmail.com>
2015-07-21 20:26:27 -0400

    tested with imgs

--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 
-setInterval(flipperz, 2000);
+setInterval(flipperz, 4200);
 function flipperz(){
-  var randIndex = Math.floor((Math.random() * 6));
+  var randIndex = Math.floor((Math.random() * 80));
+  $('.flashcard:eq('+randIndex+')').toggleClass('flipped');
+}
+
+setInterval(flipperz2, 2000);
+function flipperz2(){
+  var randIndex = Math.floor((Math.random() * 100) + 80);
+  $('.flashcard:eq('+randIndex+')').toggleClass('flipped');
+}
+
+setInterval(flipperz3, 3200);
+function flipperz3(){
+  var randIndex = Math.floor((Math.random() * 119) + 180);
   $('.flashcard:eq('+randIndex+')').toggleClass('flipped');
 }
